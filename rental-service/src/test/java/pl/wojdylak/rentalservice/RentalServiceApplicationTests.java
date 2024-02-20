7ebd9f9efb21c0bd6f79cc3568581eb0f8129310
Jan Wojdylak <jrwojdylak@gmail.com>
2024-02-20 16:15:41 +0100

    Initial services structure

---
+++ rental-service/src/test/java/pl/wojdylak/rentalservice/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojdylak.rentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
