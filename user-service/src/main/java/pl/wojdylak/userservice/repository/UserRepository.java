2df32df81c135c85e6e13cdadbbda51d9c3dfc64
Jan Wojdylak <jrwojdylak@gmail.com>
2024-04-19 10:29:14 +0200

    [user-service] Add get by id endpoint

--- user-service/src/main/java/pl/wojdylak/userservice/repository/UserRepository.java
+++ user-service/src/main/java/pl/wojdylak/userservice/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package pl.wojdylak.userservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.wojdylak.userservice.domain.User;
+import pl.wojdylak.userservice.domain.dto.UserResponseDto;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT new pl.wojdylak.userservice.domain.dto.UserResponseDto(u.id, u.firstName, u.lastName, u.email) " +
+            "FROM User u WHERE u.id = :id")
+    UserResponseDto findUserResponseDtoById(@Param("id") Long id);
 }
