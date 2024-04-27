b4548bb05768ca276a0d58bbeec668b3f28b1d4f
Jan Wojdylak <jrwojdylak@gmail.com>
2024-04-27 08:34:10 +0200

    [perf-test] Change method to closed, add run parameters

--- perf-tests/src/gatling/java/userService/UserSimulation.java
+++ perf-tests/src/gatling/java/userService/UserSimulation.java
@@ -13,12 +13,16 @@ public class UserSimulation extends Simulation {
 
     //parameters
     private static final int PAUSE_TIME_IN_SECONDS = 1;
-    private static final int USERS_COUNT = 500;
-    private static final int RAMP_DURATION = 30;
-    private static final String API_GATEWAY_IP = "34.116.141.175";
+    private static final int START_USERS_COUNT = Integer.parseInt(System.getProperty("START_USERS", "50"));
+    private static final int END_USERS_COUNT = Integer.parseInt(System.getProperty("END_USERS", "500"));
+    private static final int RAMP_DURATION = Integer.parseInt(System.getProperty("RAMP_DURATION", "500"));
+    public static final String IP_NOT_DEFINED_ERROR = "IP_NOT_DEFINED_ERROR";
+    private static final String API_GATEWAY_IP = System.getProperty("IP", IP_NOT_DEFINED_ERROR);
     private static final String URL = "http://" + API_GATEWAY_IP + ":8080";
+
     //Feeder for test data
     private static final FeederBuilder.FileBased<Object> jsonFeeder = jsonFile("data/usersData.json").random();
+
     //HTTP calls
     private static final ChainBuilder getAllUsers =
             exec(http("Get all users")
@@ -28,18 +32,20 @@ public class UserSimulation extends Simulation {
                     .exec(http("Create user")
                             .post("/users")
                             .body(ElFileBody("bodies/newUserTemplate.json")).asJson()
-                            .check(jmesPath("id").saveAs("userId")));
+                            .check(jmesPath("id").optional().saveAs("userId")));
     private static final ChainBuilder getUserById =
             exec(http("Get user by id")
                     .get("/users/#{userId}"));
     private static final ChainBuilder deleteUserById =
             exec(http("Delete user by id")
                     .delete("/users/#{userId}"));
+
     //Http configuration
     private final HttpProtocolBuilder httpProtocol = http
             .baseUrl(URL)
             .acceptHeader("application/json")
             .contentTypeHeader("application/json");
+
     //Scenario definition
     private final ScenarioBuilder scn = scenario("User service perf test")
             .exec(getAllUsers)
@@ -53,15 +59,20 @@ public class UserSimulation extends Simulation {
     //Load simulation
     {
         setUp(
-                scn.injectOpen(
-                        nothingFor(1),
-                        rampUsers(USERS_COUNT).during(RAMP_DURATION)
+                scn.injectClosed(
+                        rampConcurrentUsers(START_USERS_COUNT).to(END_USERS_COUNT).during(RAMP_DURATION)
                 )
         ).protocols(httpProtocol);
     }
 
     @Override
     public void before() {
-        System.out.println("Running test with " + USERS_COUNT + " users over " + RAMP_DURATION + " seconds");
+        System.out.println("Running test with users from " + START_USERS_COUNT + " to " + END_USERS_COUNT + " over " + RAMP_DURATION + " seconds");
+        System.out.println("API Gateway IP: " + API_GATEWAY_IP);
+
+        if (IP_NOT_DEFINED_ERROR.equals(API_GATEWAY_IP)) {
+            System.out.println("API Gateway IP is not defined. Exit tests");
+            System.exit(0);
+        }
     }
 }
