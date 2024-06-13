059a61cc4f191c542fa8340c057b26ed0adc862c
Jan Wojdylak <jrwojdylak@gmail.com>
2024-06-13 22:04:37 +0200

    [perf-tests] Add simulation for scaling up and down for user-service

--- perf-tests/src/gatling/java/services/UserServiceSimulation.java
+++ perf-tests/src/gatling/java/services/UserServiceSimulation.java
@@ -13,8 +13,8 @@ public class UserServiceSimulation extends Simulation {
 
     //parameters
     private static final int PAUSE_TIME_IN_SECONDS = 1;
-    //    private static final int START_USERS_COUNT = Integer.parseInt(System.getProperty("START_USERS", "50"));
-//    private static final int END_USERS_COUNT = Integer.parseInt(System.getProperty("END_USERS", "500"));
+    private static final int START_USERS_COUNT = Integer.parseInt(System.getProperty("START_USERS", "50"));
+    private static final int END_USERS_COUNT = Integer.parseInt(System.getProperty("END_USERS", "500"));
     private static final int RAMP_DURATION = Integer.parseInt(System.getProperty("RAMP_DURATION", "500"));
     private static final int INCREMENT_USERS = Integer.parseInt(System.getProperty("INCREMENT_USERS", "20"));
     private static final int STEPS_COUNT = Integer.parseInt(System.getProperty("STEPS_COUNT", "3"));
@@ -22,15 +22,13 @@ public class UserServiceSimulation extends Simulation {
     private static final int STEP_TIME = RAMP_DURATION / (2 * STEPS_COUNT);
     public static final String IP_NOT_DEFINED_ERROR = "IP_NOT_DEFINED_ERROR";
     private static final String API_GATEWAY_IP = System.getProperty("IP", IP_NOT_DEFINED_ERROR);
+    //    private static final String API_GATEWAY_IP = "localhost";
     private static final String URL = "http://" + API_GATEWAY_IP + ":8080";
 
     //Feeder for test data
     private static final FeederBuilder.FileBased<Object> jsonFeeder = jsonFile("data/usersData.json").random();
 
     //HTTP calls
-    private static final ChainBuilder getAllUsers =
-            exec(http("Get all users")
-                    .get("/users"));
     private static final ChainBuilder createUser =
             feed(jsonFeeder)
                     .exec(http("Create user")
@@ -52,26 +50,44 @@ public class UserServiceSimulation extends Simulation {
 
     //Scenario definition
     private final ScenarioBuilder scn = scenario("User service perf test")
-            .exec(getAllUsers)
-            .pause(PAUSE_TIME_IN_SECONDS)
             .exec(createUser)
             .pause(PAUSE_TIME_IN_SECONDS)
             .exec(getUserById)
             .pause(PAUSE_TIME_IN_SECONDS)
             .exec(deleteUserById);
 
-    //Load simulation
+    //Load simulations
+    //1 linear up
+//    {
+//        setUp(
+//                scn.injectClosed(
+//                        rampConcurrentUsers(START_USERS_COUNT).to(END_USERS_COUNT).during(RAMP_DURATION)
+//                )
+//        ).protocols(httpProtocol);
+//    }
+
+    //2 steps up
+//    {
+//        setUp(
+//                scn.injectClosed(
+//                        incrementConcurrentUsers(INCREMENT_USERS)
+//                                .times(STEPS_COUNT)
+//                                .eachLevelLasting(STEP_TIME)
+//                                .separatedByRampsLasting(STEP_TIME)
+//                                .startingFrom(0)
+//                )
+//        ).protocols(httpProtocol);
+//    }
+
+    // 3 scale up and down
     {
         setUp(
                 scn.injectClosed(
-//                        LINEAR:
-//                        rampConcurrentUsers(START_USERS_COUNT).to(END_USERS_COUNT).during(RAMP_DURATION)
-//                        STEPS:
-                        incrementConcurrentUsers(INCREMENT_USERS)
-                                .times(STEPS_COUNT)
-                                .eachLevelLasting(STEP_TIME)
-                                .separatedByRampsLasting(STEP_TIME)
-                                .startingFrom(0)
+                        constantConcurrentUsers(START_USERS_COUNT).during(240),
+                        rampConcurrentUsers(START_USERS_COUNT).to(END_USERS_COUNT).during(60),
+                        constantConcurrentUsers(END_USERS_COUNT).during(600),
+                        rampConcurrentUsers(END_USERS_COUNT).to(START_USERS_COUNT).during(60),
+                        constantConcurrentUsers(START_USERS_COUNT).during(240)
                 )
         ).protocols(httpProtocol);
     }
